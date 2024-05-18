211f32a40eb201cf76255d4f8908e9eb145ba412
Loocer <142151699+Looceri@users.noreply.github.com>
2024-05-18 23:54:18 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,21 +22,28 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    @if (Route::has('home') && Route::currentRouteName() === 'home')
+
+                    @if (!(Route::has('login') || Route::has('registar')))
                     <ul class="navbar-nav me-auto">
+
+                        @if (Route::has('criar') && Route::currentRouteName()!== 'criar')
                         <li><a class="navbar-brand" href="#">Criar</a></li>
+                        @endif
+
+                        @if (Route::has('actualizar') && Route::currentRouteName()!== 'actualizar')
                         <li><a class="navbar-brand" href="#">Actualizar</a></li>
+                        @endif
+
+                        @if (Route::has('home') && Route::currentRouteName()!== 'home')
                         <li><a class="navbar-brand" href="#">Listar</a></li>
-                        <li><a class="navbar-brand" href="#">Pesquisar</a></li>
+                        @endif
+
                     </ul>
                     @endif
 
